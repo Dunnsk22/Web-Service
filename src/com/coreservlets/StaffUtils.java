ad2b1ea5edb0a07821f688c35eab7c0e4fb3ca1d
Joshua Dunn <joshuadunn@Joshuas-MacBook-Pro.local>
2015-12-21 16:25:14 +0000

    commit 2 - MYSql stuff

--- src/com/coreservlets/StaffUtils.java
+++ src/com/coreservlets/StaffUtils.java
@@ -15,20 +15,21 @@ public class StaffUtils {
 
 	static {
 		sampleStaff = new LinkedHashMap<String, StaffInfo>();
-		sampleStaff.put("a1234", new StaffInfo(1, "James", "Gosling",
-				"5 High Street, M15 4BA", "07573929485", "JG@me.com"));
-		sampleStaff.put("a1235", new StaffInfo(2, "Jesse", "Garrett",
-				"6 High Street, M15 4BA", "1618231123", "JG@msn.com"));
-		sampleStaff.put("a1236", new StaffInfo(3, "Michael", "Bloomberg",
-				"7 High Street, M15 4BA", "07123123123", "MB@hotmail.com"));
-		sampleStaff.put("a1237", new StaffInfo(4, "Michael", "Jordan",
-				"8 High Street, M15 4BA", "07496849035", "MJ@me.com"));
-		sampleStaff.put("a1238", new StaffInfo(5, "Jon", "Bosak",
-				"8 High Street, M15 4BA", "07865432578", "JB@me.com"));
-		sampleStaff.put("a1239", new StaffInfo(6, "Alex", "Hughes",
-				"25A Albert Street, ST8 4BA", "07703343573", "hughzy11@hotmail.com"));
-		sampleStaff.put("a1240", new StaffInfo(6, "Aidan", "Oakes",
-				"19 Princess Street, ST8 G7A", "01782445663", "aidy_oakes@hotmail.com"));
+		sampleStaff = ConnectToMySQL.getStaffMembers();
+//		sampleStaff.put("a1234", new StaffInfo(1, "James", "Gosling",
+//				"5 High Street, M15 4BA", "07573929485", "JG@me.com"));
+//		sampleStaff.put("a1235", new StaffInfo(2, "Jesse", "Garrett",
+//				"6 High Street, M15 4BA", "1618231123", "JG@msn.com"));
+//		sampleStaff.put("a1236", new StaffInfo(3, "Michael", "Bloomberg",
+//				"7 High Street, M15 4BA", "07123123123", "MB@hotmail.com"));
+//		sampleStaff.put("a1237", new StaffInfo(4, "Michael", "Jordan",
+//				"8 High Street, M15 4BA", "07496849035", "MJ@me.com"));
+//		sampleStaff.put("a1238", new StaffInfo(5, "Jon", "Bosak",
+//				"8 High Street, M15 4BA", "07865432578", "JB@me.com"));
+//		sampleStaff.put("a1239", new StaffInfo(6, "Alex", "Hughes",
+//				"25A Albert Street, ST8 4BA", "07703343573", "hughzy11@hotmail.com"));
+//		sampleStaff.put("a1240", new StaffInfo(6, "Aidan", "Oakes",
+//				"19 Princess Street, ST8 G7A", "01782445663", "aidy_oakes@hotmail.com"));
 	}
 
 	public static Map<String, StaffInfo> getSampleStaff() {
